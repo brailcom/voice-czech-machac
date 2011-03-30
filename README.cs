f760177c12d2102e8a06ce76843647ec1c2e7502
Boris Dušek <dusek@brailcom.org>
2011-03-30 12:34:07 +0200

    Add release stuff from the dita voice.

---
+++ README.cs
@@ -0,0 +1,6 @@
+Toto je český dospělý mužský hlas do Festivalu.
+
+Tento produkt vznikl v rámci projektů "Olomoucká šance pro nevidomé žáky"
+(reg. č. CZ.1.07/1.2.12/02.0015) a "Centrum podpory pro zrakově postižené žáky"
+(reg. č. CZ.1.07/1.2.00/14.0111), které jsou spolufinancovány z Evropského
+sociálního fondu a státního rozpočtu České republiky.
